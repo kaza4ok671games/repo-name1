fd5ce1f180931abb1f0b4f1a8eb1161fbe262dbc
kaza4ok671games <yakobson_2015@mail.ru>
2024-12-23 05:13:56 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -86,11 +86,3 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
 });
 
 
-let usercard = document.getElementById("usercard");
-
-let p = document.createElement("p");
-
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
-
-usercard.appendChild(p);
